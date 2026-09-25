27a33b9abb90f024bf64dd11a973c23e6b3bb6d7
ayeshasheikhas0529350-prog <ayeshasheikhas0529350@gmail.com>
2026-09-25 19:47:03 +0500

    fdfd

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var name = prompt("what is your name?");
 var age = prompt("what is your age ?");
 
-window.alert = ("Hi" + name + "you are" + age + "years old" + "thanks for you input");
+window.alert = ("Hi" + name + "thanks for you input" )
+window.alert = ( "you are" + age + "years old" );
